9d33ede1a080bee9ef77ffef0b28564a7e91ecc2
DavidAlvarez34 <89229184+DavidAlvarez34@users.noreply.github.com>
2021-10-09 23:55:24 -0500

    Se agrego los archivos del backend y una carpeta del frontend

---
+++ Backend/app.js
@@ -0,0 +1,23 @@
+let express = require('express');
+let app = express();
+require('dotenv').config()
+const sequelize = require('./db/conexion');
+const userRoutes = require('./routes/user')
+
+app.use(express.json())
+async function serverStart() {
+  try {
+    await sequelize.authenticate();
+    console.log('Correct conexion');
+    app.listen(process.env.PORT, function () {
+      console.log(`Sistem start http://${process.env.HOST}:${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error('DB conexion error:', error);
+  }
+}
+
+serverStart();
+
+//Routes
+userRoutes(app)
